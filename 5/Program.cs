c98c4dce71393d1bb435eb6aa09aef276db21cb0
lisetskiy <92512252+lisetskiy@users.noreply.github.com>
2021-10-25 18:51:44 +0300

    Update Program.cs

--- 5/Program.cs
+++ 5/Program.cs
@@ -1,38 +1,31 @@
-﻿using System;
-
-namespace _5
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Задание 5");
+  Console.WriteLine("Задание 5");
             int a;
             int b;
             int S;
-            int v;
-            int vv;
+            double P0;
             int P;
             Console.Write("Введите a:");
             a = Convert.ToInt32(Console.ReadLine());
             Console.Write("Введите b:");
             b = Convert.ToInt32(Console.ReadLine());
-            if (a <= 0)
+            while (true)
             {
-                Console.WriteLine("Катет не может быть отрицательным");
-            }
-            if (b <= 0)
-            {
-                Console.WriteLine("Катет не может быть отрицательным");
-            }
+                if (a <= 0 | b <= 0)
+                {
+                    Console.WriteLine("Катет не может быть отрицательным");
+                    break;
+                }
+                else
+                {
+                    S = (a * b) / 2;
+                    Console.WriteLine("S =" + S);
+                    P0 = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+                    P = (int)(a + b + P0);
 
-            S = (a * b) / 2;
-            Console.WriteLine("S =" + S);
-            v = (int)Math.Pow(a, 2);
-            vv = (int)Math.Pow(b, 2);
-            P = (int)Math.Sqrt(v + vv);
-            Console.WriteLine("P =" + P);
+                    Console.WriteLine("P =" + P);
+                    break;
+
+                }
+            }
+           
             Console.ReadKey();
-        }
-    }
-}
