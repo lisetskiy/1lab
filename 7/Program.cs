0225ba5255728d34d62696663aaf129b0305f9fc
lisetskiy <92512252+lisetskiy@users.noreply.github.com>
2021-10-25 20:33:58 +0300

    Update Program.cs

--- 7/Program.cs
+++ 7/Program.cs
@@ -10,15 +10,28 @@ namespace _7
             
             Console.WriteLine("Введите трехзначное число");
             int n = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                if (n < 100 | n > 999)
+                {
+                    Console.WriteLine("Введите трехзначное число");
+                    break;
+                }
+                else
+                {
 
-            int a = (n / 100);
-            int b = (n / 10) % 10;
-            int c = (n % 10);
+                }
+                int a = (n / 100);
+                int b = (n / 10) % 10;
+                int c = (n % 10);
 
 
-            Console.WriteLine($"Число :{a}{b}{c}");
-            (a, b, c) = (c, b, a);
-            Console.WriteLine($"reversed : {a}{b}{c}");
+                Console.WriteLine($"Число :{a}{b}{c}");
+                (a, b, c) = (c, b, a);
+                Console.WriteLine($"reversed : {a}{b}{c}");
+                break;
+            }
+            
             Console.ReadKey();
         }
     }
